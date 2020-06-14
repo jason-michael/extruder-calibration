7a90aa23f96e7d9165073f65879f5730db973b99
Jason Michael <michael.jason@outlook.com>
2020-06-14 13:12:23 -0500

    refactor for clarity

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // UI elements
-const mMarkedDist        = document.getElementById("mark");
-const mAmountToExtruder  = document.getElementById("extrude-amt");
 const mDistFromExtToMark = document.getElementById("extruder-mark-distance");
 const mActualAmtExtruded = document.getElementById("extrude-actual");
 const mCurrentExtrusion  = document.getElementById("status");
 const mCurrentStepsPerMM = document.getElementById("steps-mm-current");
 const mCalibratedStepsMM = document.getElementById("steps-mm-new");
-const mFlow              = document.getElementById("flow");
-const mGcodeStart        = document.getElementById("gcode-start");
+const mAmountToExtrude   = document.getElementById("extrude-amt");
+const mMarkedDist        = document.getElementById("mark");
+
+const TOLERANCE = 0.5;
+let extrusionStatusColor = "#eee";
+let extrusionStatusMsg = "-";
+
 
 // Make each input call the update fn on input change
 const inputs = document.querySelectorAll("input");
@@ -15,47 +18,41 @@ inputs.forEach(input => {
     input.addEventListener("input", update);
 });
 
+/**
+ * Updates results based on input values.
+ */
 function update() {
-    // Get input values
-    const markDist = mMarkedDist.value;
-    const extAmt = mAmountToExtruder.value;
-    const markToExtDist = mDistFromExtToMark.value;
-    const stepsCurr = mCurrentStepsPerMM.value;
-    const markExtDiff = markDist - extAmt;
+    // Input values
+    const markedDist           = mMarkedDist.value;
+    const amountToExtrude      = mAmountToExtrude.value;
+    const markDistAfterExtrude = mDistFromExtToMark.value;
+    const stepsMMCurrent       = mCurrentStepsPerMM.value;
+    const markToExtruderDiff   = markedDist - amountToExtrude;
 
     // Actual amount extruded
-    let extAct = markDist - markToExtDist;
-    let extPct = ((extAct / extAmt) * 100).toFixed(1);
-    mActualAmtExtruded.textContent = extAct;
+    let actualExtrudedAmount = markedDist - markDistAfterExtrude;
+    let actualExtrudedPercent = ((actualExtrudedAmount / amountToExtrude) * 100).toFixed(1);
+    mActualAmtExtruded.textContent = actualExtrudedAmount;
 
     // Extrusion status
-    let extStatus = "-"; // Under, over, good
-    let statusColor = "#eee";
-    if (markToExtDist > markExtDiff) {
-        extStatus = "Under";
-        statusColor = "gold";
-    } else if (markToExtDist < markExtDiff) {
-        extStatus = "Over";
-        statusColor = "red";
+    if (Math.abs(markDistAfterExtrude - markToExtruderDiff) <= TOLERANCE) {
+        extrusionStatusMsg = "Good";
+        extrusionStatusColor = "limegreen";
     }
-    mCurrentExtrusion.textContent = extStatus + " (" + extPct + "%)";
-    mCurrentExtrusion.style.color = statusColor;
+    else if (markDistAfterExtrude > markToExtruderDiff) {
+        extrusionStatusMsg = "Under";
+        extrusionStatusColor = "gold";
+    }
+    else if (markDistAfterExtrude < markToExtruderDiff) {
+        extrusionStatusMsg = "Over";
+        extrusionStatusColor = "red";
+    }
+    mCurrentExtrusion.textContent = extrusionStatusMsg + " (" + Math.abs(100 - actualExtrudedPercent).toFixed(1) + "%)";
+    mCurrentExtrusion.style.color = extrusionStatusColor;
 
     // Steps/mm
-    let stepsCal = stepsCurr * extAmt / extAct;
+    let stepsCal = stepsMMCurrent * amountToExtrude / actualExtrudedAmount;
     mCalibratedStepsMM.textContent = stepsCal.toFixed(2);
-    if (stepsCal !== 0) {
-        mCalibratedStepsMM.style.color = "limegreen";
-    } else {
-        mCalibratedStepsMM.style.color = "#eee";
-    }
-
-    // Gcode start
-    const flow = mFlow.value;
-    mGcodeStart.value = `M28 \t\t\t;relative mode\nG0 E${extAmt} F${flow} \t;extrude`;
-    // Auto resize textarea rows
-    mGcodeStart.style.height = "5px";
-    mGcodeStart.style.height = (mGcodeStart.scrollHeight) + "px";
+    mCalibratedStepsMM.style.color = stepsCal !== 0 ? "limegreen" : "#eee";
 }
-
 update();
